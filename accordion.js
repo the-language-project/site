0348b699a98c2528ad63bd415c727fc02e0e991a
Amelia Dao <daoa@dickinson.edu>
2021-05-20 14:48:15 -0700

    Amelia: resources.html animation

--- accordion.js
+++ accordion.js
@@ -14,3 +14,21 @@ $(document).on("click", ".naccs .menu div", function() {
 		$(".naccs ul").height(listItemHeight + "px");
 	}
 });
+
+$(document).on("click", ".naccs2 .menu div", function() {
+	var numberIndex = $(this).index();
+
+	if (!$(this).is("active")) {
+		$(".naccs2 .menu div").removeClass("active");
+		$(".naccs2 ul li").removeClass("active");
+
+		$(this).addClass("active");
+		$(".naccs2 ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+		var listItemHeight = $(".naccs ul")
+			.find("li:eq(" + numberIndex + ")")
+			.innerHeight();
+		$(".naccs2 ul").height(listItemHeight + "px");
+	}
+});
+
